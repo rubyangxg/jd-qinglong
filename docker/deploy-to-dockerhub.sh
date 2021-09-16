6eaecb3fe7ecfa01eb48cc62ef621e273515e887
yangxg <yangxg_yang@easou.cn>
2021-09-16 13:14:08 +0800

    添加青龙容量限制

--- docker/deploy-to-dockerhub.sh
+++ docker/deploy-to-dockerhub.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-echo "是否push：$1";
-push=$1
 cd ..
 git pull --allow-unrelated-histories
 mvn clean package -Dmaven.test.skip=true
 cp target/jd-qinglong-*.jar docker/
 cd docker
 docker build -t rubyangxg/jd-qinglong:latest --build-arg JAR_FILE=jd-qinglong-1.0.jar .
-if [ push == 1 ]; then
-  docker push rubyangxg/jd-qinglong:latest
-fi
+docker push rubyangxg/jd-qinglong:latest
 
 #mvn clean package -Dmaven.test.skip=true && docker-compose -f docker-compose-debug.yml --env-file=env.properties  build --no-cache webapp
 #docker-compose -f docker-compose-debug.yml --env-file=env.properties  up -d --no-deps && docker logs -f webapp
