9492d3caea1f56989a291e4115da14187ad74c8f
yangxg <yangxg_yang@easou.cn>
2021-09-17 11:23:16 +0800

    精简镜像

--- docker-allinone/deploy-to-dockerhub.sh
+++ docker-allinone/deploy-to-dockerhub.sh
@@ -11,13 +11,13 @@ docker rmi -f rubyangxg/jd-qinglong:1.1
 cd ..
 git pull --allow-unrelated-histories
 mvn clean package -Dmaven.test.skip=true
-cp target/jd-qinglong-*.jar docker-inone
-cd docker-inone
-docker build -t rubyangxg/jd-qinglong:1.1 --build-arg JAR_FILE=jd-qinglong-1.0.jar .
+cp target/jd-qinglong-*.jar docker-allinone
+cd docker-allinone || exit
+docker build -t rubyangxg/jd-qinglong:allinone --build-arg JAR_FILE=jd-qinglong-1.0.jar .
 
-docker run --name=jd1.1 rubyangxg/jd-qinglong:1.1
+docker run --name=jd-qinglong rubyangxg/jd-qinglong:allinone
 if [ $op == 'push' ]; then
-   docker push rubyangxg/jd-qinglong:1.1
+   docker push rubyangxg/jd-qinglong:allinone
 fi
 
 #mvn clean package -Dmaven.test.skip=true && docker-compose -f docker-compose-debug.yml --env-file=env.properties  build --no-cache webapp
