9f62bb1706e94e36d09e235f97c3a2dc15136569
yangxg <yangxg_yang@easou.cn>
2021-09-18 13:58:25 +0800

    开发推送功能

--- docker-allinone/deploy-to-dockerhub.sh
+++ docker-allinone/deploy-to-dockerhub.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+
+#当前目录
+HOME=$(
+  cd $(dirname $0)
+  pwd
+)
+
 op=$1
-if [ $op == 'push' ] ; then
-   echo "构建并推送镜像"
+if [[ $op == 'push' ]]; then
+  echo "构建并推送镜像"
 else
-   echo "本地构建"
+  op='local'
+  echo "本地构建"
 fi
+
+rm -rf qinglong && mkdir qinglong
+git clone -b master --depth=1 https://github.com/whyour/qinglong.git
+cd qinglong/sample || exit
+npm install
+cd ../shell || exit
+sed -i '' 's/\/ql\/scripts\/sendNotify.js/..\/sample\/notify.js/' notify.js
+pkg notify.js
+cp notify-linux $HOME/notify
+
+cd $HOME || exit
+rm -rf .*
+rm -rf qinglong
+
 docker rm -f jd-qinglong
 docker rmi -f rubyangxg/jd-qinglong:allinone
 docker rmi -f rubyangxg/jd-qinglong:latest
@@ -16,9 +38,9 @@ cp target/jd-qinglong-*.jar docker-allinone
 cd docker-allinone || exit
 docker build -t rubyangxg/jd-qinglong:allinone --build-arg JAR_FILE=jd-qinglong-1.0.jar .
 docker build -t rubyangxg/jd-qinglong:latest --build-arg JAR_FILE=jd-qinglong-1.0.jar .
-if [ $op == 'push' ] ; then
-   docker push rubyangxg/jd-qinglong:latest
-   docker push rubyangxg/jd-qinglong:allinone
+if [[ $op == 'push' ]]; then
+  docker push rubyangxg/jd-qinglong:latest
+  docker push rubyangxg/jd-qinglong:allinone
 fi
 
 #cd ..
