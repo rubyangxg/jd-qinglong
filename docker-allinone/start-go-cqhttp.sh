da8f09ad3e775fce463eb87930aacde605892974
yangxg <yangxg_yang@easou.cn>
2021-09-26 21:48:46 +0800

    更新README

---
+++ docker-allinone/start-go-cqhttp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+file=/go-cqhttp/go-cqhttp
+if [ ! -f "$file" ]; then
+  wget -O "go-cqhttp.tar.gz" https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.0-beta7-fix2/go-cqhttp_linux_amd64.tar.gz
+  tar -zxvf go-cqhttp.tar.gz -C /go-cqhttp/
+  chmod +x /go-cqhttp/*
+  cp /config-template.yml /go-cqhttp/config.yml
+else
+  echo "go-cqhttp已存在"
+fi
+
+sh /go-cqhttp/go-cqhttp -w /go-cqhttp
