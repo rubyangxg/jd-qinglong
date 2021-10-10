e658b93b3d27475a5179bbb8dab5a57e027a34aa
yangxg <yangxg_yang@easou.cn>
2021-10-10 15:34:09 +0800

    重构代码

--- docker-allinone/deploy-to-dockerhub.sh
+++ docker-allinone/deploy-to-dockerhub.sh
@@ -64,9 +64,7 @@ rm -rf qinglong
 
 docker rm -f webapp
 docker rmi -f rubyangxg/jd-qinglong:latest
-docker rmi -f rubyangxg/jd-qinglong:1.2
-
-
+docker rmi -f rubyangxg/jd-qinglong:1.3
 
 if [ ! -f "$HOME/jd-qinglong-1.0.jar" ];then
   cd ..
@@ -79,16 +77,16 @@ else
 fi
 
 docker build -t rubyangxg/jd-qinglong:latest --build-arg JAR_FILE=jd-qinglong-1.0.jar .
-docker build -t rubyangxg/jd-qinglong:1.2 --build-arg JAR_FILE=jd-qinglong-1.0.jar .
+docker build -t rubyangxg/jd-qinglong:1.3 --build-arg JAR_FILE=jd-qinglong-1.0.jar .
 if [[ $op == 'push' ]]; then
   docker login
-  docker push rubyangxg/jd-qinglong:latest && docker push rubyangxg/jd-qinglong:1.2
+  docker push rubyangxg/jd-qinglong:latest && docker push rubyangxg/jd-qinglong:1.3
 fi
 
 rm -rf $HOME/.docker
 cd ..
-docker stop webapp && docker rm webapp && docker rmi rubyangxg/jd-qinglong:1.2
-#docker run -d -p 5701:8080 -p 4444:4444 -p 8001:8001 --name=webapp --privileged=true -e "SE_NODE_MAX_SESSIONS=8" -e "SPRING_PROFILES_ACTIVE=debugremote" -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)"/env.properties:/env.properties:ro -v "$(pwd)"/go-cqhttp:/go-cqhttp rubyangxg/jd-qinglong:1.2
+docker stop webapp && docker rm webapp && docker rmi rubyangxg/jd-qinglong:1.3
+#docker run -d -p 5701:8080 -p 4444:4444 -p 8001:8001 --name=webapp --privileged=true -e "SE_NODE_MAX_SESSIONS=8" -e "SPRING_PROFILES_ACTIVE=debugremote" -v /var/run/docker.sock:/var/run/docker.sock -v "$(pwd)"/env.properties:/env.properties:ro -v "$(pwd)"/go-cqhttp:/go-cqhttp rubyangxg/jd-qinglong:1.3
 
 #mvn clean package -Dmaven.test.skip=true && docker-compose -f docker-compose-debug.yml --env-file=env.properties  build --no-cache webapp
 #docker-compose -f docker-compose-debug.yml --env-file=env.properties  up -d --no-deps && docker logs -f webapp
