5ebd3ad43e1245850a68fe7c41b3f74ea009d1c3
yangxg <yangxg_yang@easou.cn>
2021-10-11 21:20:48 +0800

    重构代码

--- docker-allinone/deploy-to-dockerhub.sh
+++ docker-allinone/deploy-to-dockerhub.sh
@@ -86,7 +86,6 @@ if [[ $op == 'push' ]]; then
   docker push rubyangxg/jd-qinglong:1.3
 fi
 
-rm -rf $HOME/.docker
 cd /root/run
 docker stop webapp && docker rm webapp && docker rmi rubyangxg/jd-qinglong:1.3
 docker run -d -p 5701:8080 -p 8001:8001 --name=webapp --privileged=true -e "SPRING_PROFILES_ACTIVE=debugremote" -v "$(pwd)"/env.properties:/env.properties:ro -v "$(pwd)"/go-cqhttp:/go-cqhttp rubyangxg/jd-qinglong:1.3
