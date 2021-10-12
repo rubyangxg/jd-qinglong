cff24d0ce95055772a2bf66ecac8acbf164b34a7
yangxg <yangxg_yang@easou.cn>
2021-10-12 23:21:19 +0800

    重构代码

--- src/main/java/com/meread/selenium/util/SlideVerifyBlock.java
+++ src/main/java/com/meread/selenium/util/SlideVerifyBlock.java
@@ -144,7 +144,7 @@ public class SlideVerifyBlock {
             if (current < distance * 0.7) {
                 a = A_SPEED;
             } else {
-                a = -200;
+                a = -400;
             }
             double dis = 0.5 * a * Math.pow((cnt + 1) * (0.1), 2);
             list.add(Double.valueOf(df.format(dis - current)));
